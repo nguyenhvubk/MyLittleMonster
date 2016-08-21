4f4ab5784289d24a23d73aa5eb1e846274763cdb
Hoang Vu <nguyenhvubk@gmail.com>
2016-08-21 17:42:32 +0900

    Drag Image

--- MyLittleMonster/ViewController.swift
+++ MyLittleMonster/ViewController.swift
@@ -11,14 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var imgArr: [UIImage]
-        imgArr = [UIImage]()
+        var imgArr = [UIImage]()
         for x in 1...4 {
             let img = UIImage(named: "idle\(x).png")
-//          imgArr.append(img)
+//            imgArr.append(img)
             if let _img = img {
                 imgArr.append(_img)
             }
